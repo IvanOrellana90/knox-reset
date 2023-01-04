dcb1a4d2b1625a2b3b7919243ddb4f39759e5162
Ivan Orellana <ivan990@gmail.com>
2023-01-04 20:56:37 -0300

    feat(slack-api): remove headers and replace env slack url

--- src/lib/slack-api.js
+++ src/lib/slack-api.js
@@ -1,27 +1,22 @@
 export async function sendMessageBotRestart(tag) {
   console.log("Using Slack API " + tag);
-  let myHeaders = new Headers({
-    "Content-Type": "text/xml",
-  });
 
   try {
-    const response = await fetch(
-      "https://hooks.slack.com/services/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",
-      {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text: "<@U01TNL05BE2> knox restart " + tag }),
-      }
-    );
+    const response = await fetch(process.env.REACT_APP_SLACK_URL, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: "<@U01TNL05BE2> knox restart " + tag }),
+    });
     if (response.ok) {
-      console.log("Todo bien");
+      return "Todo bien con el reinicio";
     } else {
-      console.log("Respuesta de red OK pero respuesta de HTTP no OK");
+      return "Respuesta de red OK pero respuesta de HTTP no OK";
     }
   } catch (error) {
-    console.log("Hubo un problema con la petición Fetch:" + error.message);
+    console.log(error);
+    return "Hubo un problema con la petición Fetch";
   }
 }
